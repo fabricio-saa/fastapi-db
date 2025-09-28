0723f7c5c9221668c25bc31d72ce921de7eed540
fabricio-saa <fabricio.saavedra91@gmail.com>
2025-09-28 00:01:03 -0300

    Fix engine creation

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ sqlite_file_name = "database.db"
 sqlite_url = f'sqlite:///{sqlite_file_name}'
 
 connect_args = {'check_same_thread': False}
-engine = create_engine(sqlite_file_name, connect_args=connect_args) # this is what holds the connection to the db
+engine = create_engine(sqlite_url, connect_args=connect_args) # this is what holds the connection to the db
 # only need one db in this case so we only have one engine
 
 def create_db_and_tables():
