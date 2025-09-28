d9ccdfcf21e6774833287de35ae3ad10f99de536
fabricio-saa <fabricio.saavedra91@gmail.com>
2025-09-27 23:45:30 -0300

    Add SessionDep shortcut for endpoints and on_startup definition

--- main.py
+++ main.py
@@ -18,4 +18,17 @@ engine = create_engine(sqlite_file_name, connect_args=connect_args) # this is wh
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-    
+
+# the session allows to store the objects in memory
+# this will be injected as a dependency so that each request has a session to communicate with the db through the engine
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
