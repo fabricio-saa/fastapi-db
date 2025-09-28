9d6db897464d05d268febd5d9397d37067e97255
fabricio-saa <fabricio.saavedra91@gmail.com>
2025-09-28 00:11:48 -0300

    Add hero base model

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+
+class HeroBase(SQLModel):
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+
+
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
