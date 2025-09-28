863b07f106f9b4ce94f79dc15cf589887f5cc15e
fabricio-saa <fabricio.saavedra91@gmail.com>
2025-09-28 00:28:21 -0300

    Add update_hero endpoint

--- main.py
+++ main.py
@@ -76,3 +76,16 @@ def read_hero(hero_id: int, session: SessionDep):
     if not hero:
         raise HTTPException(status_code=404, detail='Hero not found')
     return hero
+
+
+@app.patch('/heroes/{hero_id}', response_model=HeroPublic)
+def update_hero(hero_id: int, session: SessionDep, hero: HeroUpdate):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail='Hero not found')
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
